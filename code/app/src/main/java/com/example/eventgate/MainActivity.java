ca5fb6937f65fda6473d6f5c2e2198ded63c6df7
jsu1261 <jennysu12@outlook.com>
2024-02-29 13:02:09 -0700

    Add navigation to other activities

--- code/app/src/main/java/com/example/eventgate/MainActivity.java
+++ code/app/src/main/java/com/example/eventgate/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.eventgate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.example.eventgate.admin.AdminActivity;
+import com.example.eventgate.attendee.AttendeeActivity;
+import com.example.eventgate.organizer.OrganizerActivity;
 
 public class MainActivity extends AppCompatActivity {
+    Button attendeeButton;
+    Button organizerButton;
+    Button adminButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        attendeeButton = findViewById(R.id.attendee_button);
+        organizerButton = findViewById(R.id.organizer_button);
+        adminButton = findViewById(R.id.admin_button);
+
+        // Start each activity with an intent.
+        attendeeButton.setOnClickListener(v -> startActivity(new Intent(MainActivity.this,
+                AttendeeActivity.class)));
+
+        organizerButton.setOnClickListener(v -> startActivity(new Intent(MainActivity.this,
+                OrganizerActivity.class)));
+
+        adminButton.setOnClickListener(v -> startActivity(new Intent(MainActivity.this,
+                AdminActivity.class)));
     }
 }
