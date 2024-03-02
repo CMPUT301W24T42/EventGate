a7334b7812dcde7a9618ada2dd5e2faf199bf4df
jsu1261 <jennysu12@outlook.com>
2024-03-02 16:07:07 -0700

    Added anonymous authentication

--- code/app/build.gradle.kts
+++ code/app/build.gradle.kts
@@ -38,6 +38,7 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation(platform("com.google.firebase:firebase-bom:32.7.3"))
+    implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-analytics")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
