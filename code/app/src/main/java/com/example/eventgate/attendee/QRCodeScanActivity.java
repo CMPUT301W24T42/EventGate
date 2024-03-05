92009f983906ccb0c4412a88420ca76a337b1666
Colton Schneider <colton@pambra.ca>
2024-03-04 19:40:40 -0700

    Created QR Code Scanning activity

---
+++ code/app/src/main/java/com/example/eventgate/attendee/QRCodeScanActivity.java
@@ -0,0 +1,73 @@
+package com.example.eventgate.attendee;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class QRCodeScanActivity extends AppCompatActivity {
+
+    private static final int PERMISSION_REQUEST_CAMERA = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA},
+            PERMISSION_REQUEST_CAMERA);
+        } else {
+            initQRCodeScan();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == PERMISSION_REQUEST_CAMERA) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                initQRCodeScan();
+            } else {
+                Toast.makeText(this, "Camera permission is required",
+                        Toast.LENGTH_LONG).show();
+                finish();
+            }
+        }
+    }
+
+    private void initQRCodeScan() {
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE);
+        integrator.setOrientationLocked(true);
+        integrator.setPrompt("Scan a QR Code");
+        integrator.initiateScan();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents() == null) {
+                Toast.makeText(this, "Scan Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Scanned: " + result.getContents(),
+                        Toast.LENGTH_LONG).show();
+            }
+        } else {
+            super.onActivityResult(resultCode, resultCode, data);
+        }
+    }
+
+}
