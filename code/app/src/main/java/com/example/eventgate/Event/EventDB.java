d58072eadd10dc66463779fa14796b96796309af
Christian Eddie <ceddie1@ualberta.ca>
2024-03-06 16:13:53 -0700

    Working on adding data to the database with new EventDB class.

--- code/app/src/main/java/com/example/eventgate/Event/EventDB.java
+++ code/app/src/main/java/com/example/eventgate/Event/EventDB.java
@@ -1,11 +1,15 @@
 package com.example.eventgate.Event;
 
+import android.graphics.Bitmap;
 import android.util.Log;
 
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * This is used to add, remove, and retrieve event data from the database
@@ -42,6 +46,35 @@ public class EventDB {
         HashMap<String, String> data = new HashMap<>();
         data.put("eventId", event.getEventId());
         data.put("name", event.getEventName());
+        collection
+                .document(eventId)
+                .set(data)
+                .addOnSuccessListener(unused -> Log.d(TAG, "Event has been added successfully!"))
+                .addOnFailureListener(e -> Log.d(TAG, "Event could not be added!" + e));
+    }
+
+    public void AddOrganizerEvent(Event event, Bitmap eventQRBitmap) {
+        // Convert bitmap to byte array
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        eventQRBitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
+        byte[] byteArray = baos.toByteArray();
+
+        // Convert byte array to list of integers
+        List<Integer> byteArrayAsList = new ArrayList<>();
+        for (byte b : byteArray) {
+            byteArrayAsList.add((int) b);
+        }
+
+        String eventId = collection.document().getId();
+        event.setEventId(eventId);
+
+        HashMap<String, String> data = new HashMap<>();
+        data.put("eventId", event.getEventId());
+        data.put("name", event.getEventName());
+        data.put("checkInQRCode", byteArrayAsList.toString());
+        data.put("organizer", ""); // Set organizer field to blank
+        data.put("attendees", ""); // Set attendees field to blank
+
         collection
                 .document(eventId)
                 .set(data)
