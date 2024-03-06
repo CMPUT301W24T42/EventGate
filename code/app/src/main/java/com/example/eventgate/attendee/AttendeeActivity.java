d691c27408180ca7d2cf7372089828b81c19b3f7
JasonZhai-9936 <haotai@ualberta.ca>
2024-03-06 14:33:36 -0700

    Upload and remove profile pictures functional

--- code/app/src/main/java/com/example/eventgate/attendee/AttendeeActivity.java
+++ code/app/src/main/java/com/example/eventgate/attendee/AttendeeActivity.java
@@ -2,14 +2,21 @@ package com.example.eventgate.attendee;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.AbsoluteSizeSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,12 +25,28 @@ import com.example.eventgate.R;
 
 //citations
 //https://stackoverflow.com/questions/44131469/android-using-shared-preferences-to-check-on-first-run
+//https://stackoverflow.com/questions/16335178/different-font-size-of-strings-in-the-same-textview
+
 
 public class AttendeeActivity extends AppCompatActivity {
+
+    private static final int PICK_IMAGE = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_attendee);
+
+        //some UI adjustments
+
+        //text sizing for events listview title
+        TextView textView = findViewById(R.id.EventListViewTitle);
+        String text = "Attendee Menu-> Your Events";
+        SpannableString spannableString = new SpannableString(text);
+        spannableString.setSpan(new AbsoluteSizeSpan(13, true), 0, 13, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannableString.setSpan(new AbsoluteSizeSpan(20, true), 15, text.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        textView.setText(spannableString);
+
 
         //check if first time opening attendee section, save attendee to db if so
         SharedPreferences prefs = getSharedPreferences("MyAppPrefs", MODE_PRIVATE);
@@ -33,6 +56,7 @@ public class AttendeeActivity extends AppCompatActivity {
             user_settings_dialog();
 
         }
+
 
 
 
@@ -55,6 +79,24 @@ public class AttendeeActivity extends AppCompatActivity {
             }
         });
 
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE && resultCode == RESULT_OK) {
+            if (data == null) {
+                // Display an error
+                return;
+            }
+            try {
+                ImageButton imageButton = findViewById(R.id.profile_image);
+                Uri imageUri = data.getData();
+                imageButton.setImageURI(imageUri);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     //Popup for user settings when gear icon clicked
@@ -107,10 +149,14 @@ public class AttendeeActivity extends AppCompatActivity {
         builder.setTitle("Profile Picture");
 
 
+
+
         builder.setPositiveButton("Upload New Profile Picture", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                //open file explorer, handle new image
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, PICK_IMAGE);
             }
         });
 
@@ -118,6 +164,8 @@ public class AttendeeActivity extends AppCompatActivity {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 //remove current profile picture
+                ImageButton imageButton = findViewById(R.id.profile_image);
+                imageButton.setImageDrawable(null);
             }
         });
 
