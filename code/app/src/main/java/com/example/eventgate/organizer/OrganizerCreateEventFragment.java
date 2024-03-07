a3572795b89ec98ca2680249c0a75f7c3f8c9ad7
Christian Eddie <ceddie1@ualberta.ca>
2024-03-06 19:50:01 -0700

    Finished incorporating new EventDB class. An organizer can now add an event and generate a QR Code. This data is now saved to the DB.

--- code/app/src/main/java/com/example/eventgate/organizer/OrganizerCreateEventFragment.java
+++ code/app/src/main/java/com/example/eventgate/organizer/OrganizerCreateEventFragment.java
@@ -16,9 +16,8 @@ import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
-import com.example.eventgate.Event.Event;
-import com.example.eventgate.R;
 import com.example.eventgate.event.Event;
+import com.example.eventgate.R;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
@@ -36,8 +35,6 @@ public class OrganizerCreateEventFragment extends DialogFragment {
      * Interface definition for a callback to be invoked when an event is added.
      */
     public interface OnEventAddedListener {
-        void onEventAdded(Event event, Bitmap eventQRBitmap);
-
         void onEventAdded(Event event, Bitmap eventQRBitmap);
     }
 
