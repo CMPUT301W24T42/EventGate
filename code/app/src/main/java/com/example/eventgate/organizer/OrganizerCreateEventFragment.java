73fe405f9f837b5301b162949ce67e95786bdcd0
Christian Eddie <ceddie1@ualberta.ca>
2024-03-07 16:00:56 -0700

    Organizer can now generate an Event Description QR Code, but it does not yet get added to Firebase.

--- code/app/src/main/java/com/example/eventgate/organizer/OrganizerCreateEventFragment.java
+++ code/app/src/main/java/com/example/eventgate/organizer/OrganizerCreateEventFragment.java
@@ -7,6 +7,7 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -18,14 +19,23 @@ import androidx.annotation.Nullable;
 
 import com.example.eventgate.event.Event;
 import com.example.eventgate.R;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 /**
  * A fragment for creating a new event.
  */
 public class OrganizerCreateEventFragment extends DialogFragment {
     private Boolean qRCodeGenerated = false;
+    private Boolean descriptionQRCodeGenerated = false;
     private Bitmap eventQRBitmap;
+    private Bitmap descriptionQRBitmap;
     private OnQRCodeGeneratedListener qrCodeListener;
+    private OnQRCodeGeneratedListener descriptionQRCodeListener;
+    private Event eventAdded;
 
     /**
      * Interface definition for a callback to be invoked when an event is added.
@@ -77,8 +87,10 @@ public class OrganizerCreateEventFragment extends DialogFragment {
         Button continueButton = view.findViewById(R.id.organizerCreateEventContinueButton);
         Button cancelButton = view.findViewById(R.id.organizerCreateEventCancelButton);
         EditText organizerCreateEventName = view.findViewById(R.id.organizerCreateEventName);
-        ImageView qRCode = view.findViewById(R.id.organizerCreateEventQRCode);
+        ImageView checkInQRCode = view.findViewById(R.id.organizerCreateEventQRCode);
+        ImageView descriptionQRCode = view.findViewById(R.id.organizerEventDescriptionQRCode);
         Button generateQRButton = view.findViewById(R.id.generateQRButton);
+        Button generateDescriptionQRButton = view.findViewById(R.id.generateDescriptionQRButton);
 
         generateQRButton.setOnClickListener(v -> {
             // Only generate one QR Code
@@ -97,14 +109,53 @@ public class OrganizerCreateEventFragment extends DialogFragment {
                 }
 
                 // Create an Event object
-                Event event = new Event(eventName);
+                eventAdded = new Event(eventName);
 
                 if (getActivity() instanceof OnEventAddedListener) {
                     // Pass the event name and listener to handle the QR code bitmap
-                    ((OnEventAddedListener) getActivity()).onEventAdded(event, eventQRBitmap -> {
-                        qRCode.setImageBitmap(eventQRBitmap);
+                    ((OnEventAddedListener) getActivity()).onEventAdded(eventAdded, eventQRBitmap -> {
+                        checkInQRCode.setImageBitmap(eventQRBitmap);
                         qRCodeGenerated = true;
                     });
+                }
+            }
+        });
+
+        generateDescriptionQRButton.setOnClickListener(v -> {
+            if (!descriptionQRCodeGenerated) {
+                // Check In QR Code must be generated first because it adds the event
+                if (qRCodeGenerated) {
+                    if (eventAdded != null) {
+                        // Create Event Description QR Code
+                        MultiFormatWriter writer = new MultiFormatWriter();
+                        Log.d("DescriptionQRCode", "Made it passed 1.");
+
+                        try {
+                            BitMatrix matrix = writer.encode(eventAdded.getEventName(), BarcodeFormat.QR_CODE, 400, 400);
+                            Log.d("DescriptionQRCode", "Made it passed 2.");
+                            BarcodeEncoder encoder = new BarcodeEncoder();
+                            Log.d("DescriptionQRCode", "Made it passed 3.");
+                            descriptionQRBitmap = encoder.createBitmap(matrix);
+                            Log.d("DescriptionQRCode", "Made it passed 4.");
+
+                        } catch (WriterException e) {
+                            Log.d("DescriptionQRCode", "Made it passed 5.");
+                            e.printStackTrace();
+                        }
+
+                        Log.d("DescriptionQRCode", "Made it passed 6.");
+
+                        eventAdded.setEventQRBitmap(eventQRBitmap);
+                        Log.d("DescriptionQRCode", "Made it passed 7.");
+                        descriptionQRCode.setImageBitmap(descriptionQRBitmap);
+                        Log.d("DescriptionQRCode", "Made it passed 8.");
+                        descriptionQRCodeGenerated = true;
+                        Log.d("DescriptionQRCode", "Made it passed 9.");
+                    } else {
+                        Toast.makeText(getActivity(), "Please generate Check In QR Code first.", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(getActivity(), "Please generate Check In QR Code first.", Toast.LENGTH_SHORT).show();
                 }
             }
         });
