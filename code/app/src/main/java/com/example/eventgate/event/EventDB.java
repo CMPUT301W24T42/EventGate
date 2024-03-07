3d9be2955b2fcd3da6cfae760d50337674cd474c
Christian Eddie <ceddie1@ualberta.ca>
2024-03-06 20:12:47 -0700

    Fixed issues.

--- code/app/src/main/java/com/example/eventgate/event/EventDB.java
+++ code/app/src/main/java/com/example/eventgate/event/EventDB.java
@@ -33,8 +33,8 @@ public class EventDB {
      * Constructs a new EventDB
      */
     public EventDB() {
-        db = MainActivity.db.getDB();
-        collection = MainActivity.db.getEventsRef();
+        db = FirebaseFirestore.getInstance();
+        collection = db.collection("events");
     }
 
     /**
@@ -42,7 +42,7 @@ public class EventDB {
      *
      * @param event the event to add
      */
-    public void addEvent(com.example.eventgate.event.Event event) {
+    public void addEvent(Event event) {
         String eventId = collection.document().getId();
         event.setEventId(eventId);
         HashMap<String, String> data = new HashMap<>();
@@ -61,7 +61,7 @@ public class EventDB {
      * @param event         The event object containing details of the event.
      * @param eventQRBitmap The bitmap image of the event's QR code.
      */
-    public void AddOrganizerEvent(com.example.eventgate.event.Event event, Bitmap eventQRBitmap) {
+    public void AddOrganizerEvent(Event event, Bitmap eventQRBitmap) {
         // Convert bitmap to byte array
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         eventQRBitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
@@ -76,14 +76,12 @@ public class EventDB {
         String eventId = collection.document().getId();
         event.setEventId(eventId);
 
-        String[] attendees = new String[0];
-
-        HashMap<String, Object> data = new HashMap<>();
+        HashMap<String, String> data = new HashMap<>();
         data.put("eventId", event.getEventId());
         data.put("name", event.getEventName());
         data.put("checkInQRCode", byteArrayAsList.toString());
         data.put("organizer", ""); // Set organizer field to blank
-        data.put("attendees", attendees); // Set attendees field to blank
+        data.put("attendees", ""); // Set attendees field to blank
 
         collection
                 .document(eventId)
@@ -97,7 +95,7 @@ public class EventDB {
      *
      * @param event the event to remove
      */
-    public void removeEvent(com.example.eventgate.event.Event event) {
+    public void removeEvent(Event event) {
         String eventId = event.getEventId();
         collection.document(eventId)
                 .delete()
