0a0ebce21fbb72ce5638f7bd895c178a99584719
Christian Eddie <ceddie1@ualberta.ca>
2024-03-08 12:24:57 -0700

    Worked on test cases.

--- code/app/src/androidTest/java/com/example/eventgate/OrganizerMainMenuActivityTest.java
+++ code/app/src/androidTest/java/com/example/eventgate/OrganizerMainMenuActivityTest.java
@@ -5,23 +5,17 @@ import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
 import androidx.test.core.app.ActivityScenario;
-
-import com.example.eventgate.organizer.OrganizerEventEditorActivity;
 import com.example.eventgate.organizer.OrganizerMainMenuActivity;
 
 import org.junit.Rule;
 import org.junit.Test;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
 
-import static androidx.test.espresso.Espresso.onData;
-import static org.hamcrest.Matchers.instanceOf;
-import static org.hamcrest.Matchers.is;
-
-import androidx.test.espresso.intent.Intents;
-import androidx.test.espresso.intent.matcher.IntentMatchers;
 import androidx.test.espresso.intent.rule.IntentsTestRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-
-import com.example.eventgate.event.Event;
+import androidx.test.espresso.action.ViewActions;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 import org.junit.runner.RunWith;
 
@@ -57,20 +51,26 @@ public class OrganizerMainMenuActivityTest {
     }
 
     /**
-     * Test to verify that clicking on an item in the event list opens the event editor activity
-     * with the correct event ID and name as extras in the intent.
+     * Espresso test method to verify that an event called "Event1" is added.
      */
     @Test
-    public void testEventListItemClicked_OpensEventEditorActivity() {
-        // Click on the first item in the event list
-        onData(is(instanceOf(Event.class)))
-                .inAdapterView(withId(R.id.EventListView))
-                .atPosition(0)
-                .perform(click());
+    public void testAddEventCalledEvent1() {
+        try (ActivityScenario<OrganizerMainMenuActivity> scenario = ActivityScenario.launch(OrganizerMainMenuActivity.class)) {
+            // Click on the Create Event Button
+            onView(withId(R.id.CreateEventButton)).perform(click());
 
-        // Verify that the intent to open the event editor activity is sent with correct extras
-        Intents.intended(IntentMatchers.hasComponent(OrganizerEventEditorActivity.class.getName()));
-        Intents.intended(IntentMatchers.hasExtra("eventId", "AtvaEq6KTuDcEgim7URQ")); // This is where you enter the expected eventId
-        Intents.intended(IntentMatchers.hasExtra("eventName", "Event 1")); // This is where you enter the expected eventName
+            // Enter the event name "Event1"
+            onView(withId(R.id.organizerCreateEventName)).perform(ViewActions.typeText("Event1"), ViewActions.closeSoftKeyboard());
+
+            // Generate both QR Codes
+            onView(withId(R.id.generateQRButton)).perform(click());
+            onView(withId(R.id.generateDescriptionQRButton)).perform(click());
+
+            // Click on Continue Button
+            onView(withId(R.id.organizerCreateEventContinueButton)).perform(click());
+
+            // Verify that the event "Event1" is added to the list
+            onView(withText("Event1")).check(matches(isDisplayed()));
+        }
     }
 }
