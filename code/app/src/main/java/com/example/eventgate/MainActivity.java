0f795bbf75e78363bd2e28c07fb8523f9888c6c5
jsu1261 <jennysu12@outlook.com>
2024-03-14 14:26:38 -0600

    remove admin sdk

--- code/app/src/main/java/com/example/eventgate/MainActivity.java
+++ code/app/src/main/java/com/example/eventgate/MainActivity.java
@@ -73,8 +73,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FirebaseApp.initializeApp(this);  // initialize the firebase admin sdk
-
         // ask the user to permission to receive notifications from the app
         askNotificationPermission();
 
