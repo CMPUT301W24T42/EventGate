3785fb17ab852a3bb2aa0b89eb49d2e8bfc45a63
jsu1261 <jennysu12@outlook.com>
2024-03-22 20:35:58 -0600

    created admin event viewer ui

---
+++ code/app/src/main/java/com/example/eventgate/admin/AdminEventViewerActivity.java
@@ -0,0 +1,16 @@
+package com.example.eventgate.admin;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.eventgate.R;
+
+public class AdminEventViewerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_event_viewer);
+    }
+}
