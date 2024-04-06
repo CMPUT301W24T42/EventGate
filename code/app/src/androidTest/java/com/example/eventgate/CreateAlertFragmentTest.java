2682328f273fb069d1022237ca8268cd959908b4
jsu1261 <jennysu12@outlook.com>
2024-04-06 13:08:16 -0600

    update createalertfragmenttest

--- code/app/src/androidTest/java/com/example/eventgate/CreateAlertFragmentTest.java
+++ code/app/src/androidTest/java/com/example/eventgate/CreateAlertFragmentTest.java
@@ -3,6 +3,7 @@ package com.example.eventgate;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 import android.os.Bundle;
 
@@ -23,21 +24,17 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class CreateAlertFragmentTest {
     @Before
-    public void setUp() throws InterruptedException {
+    public void setUp() {
         // fragment requires data from activity so create a bundle with test data
         Bundle args = new Bundle();
         args.putString("eventId", "lRjkvRKnUmHL4432qLYw");
         // Launch the fragment with the bundle
-        FragmentScenario.launchInContainer(CreateAlertFragment.class, args, R.style.Theme_EventGate).moveToState(Lifecycle.State.CREATED);
-        // wait for fragment to pop up
-        Thread.sleep(2000);
-
-
+        FragmentScenario.launch(CreateAlertFragment.class, args, R.style.Theme_EventGate);
     }
     @Test
     public void testCancelButton() {
         // click cancel button
-        Espresso.onView(withId(R.id.create_alert_cancel_button)).perform(ViewActions.click());
+        Espresso.onView(withText("Cancel")).perform(ViewActions.click());
         // check that OrganizerEventEditorActivity is now shown
         Espresso.onView(withId(R.id.eventContainer)).check(matches(isDisplayed()));
     }
